b8fe62e458eae08bd304e98acb0038fc06e527da
Magnus von Koeller <magnus@vonkoeller.de>
2013-08-21 13:59:17 -0400

    allow for excluding some requests from tracking

--- app/controllers/felon/track_controller.rb
+++ app/controllers/felon/track_controller.rb
@@ -1,16 +1,16 @@
-class Felon::TrackController < ActionController::Base
+class Felon::TrackController < ApplicationController
   def view
-    Felon::Counter.record_view(params[:variant_id])
+    Felon::Counter.record_view(params[:variant_id]) if track?
     send_blank_response
   end
 
   def interaction
-    Felon::Counter.record_interaction(params[:variant_id])
+    Felon::Counter.record_interaction(params[:variant_id]) if track?
     send_blank_response
   end
 
   def conversion
-    Felon::Counter.record_conversion(params[:variant_id])
+    Felon::Counter.record_conversion(params[:variant_id]) if track?
     send_blank_response
   end
 
@@ -25,6 +25,15 @@ class Felon::TrackController < ActionController::Base
       format.any do
         send_data(Base64.decode64("R0lGODlhAQABAPAAAAAAAAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="), :type => "image/gif", :disposition => "inline")
       end
+    end
+  end
+
+  def track?
+    @has_felon_track ||= self.respond_to?(:felon_track?)
+    if @has_felon_track
+      felon_track?
+    else
+      true
     end
   end
 end
